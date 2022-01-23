f5ec403a287d1cc5bd22cdab9deef0d2f15efe39
Daniel Foley <32227725+FunsizeEvil@users.noreply.github.com>
2022-01-23 15:23:02 +0000

    Login and Register Basic UI implemented
    Added basic UI and option in main activity to click the login page. This will later be changed to a system that checks if a user, is logged in or not.

---
+++ app/src/main/java/com/example/farmmanagerhelper/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.farmmanagerhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button openRegisterAccountActivity = findViewById((R.id.btnRegisterNewUserAcitivty));
+
+        Button logUserIn = findViewById(R.id.btnLoginUser);
+
+        openRegisterAccountActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Class activityClass = com.example.farmmanagerhelper.RegisterAccountActivity.class;
+                Intent intent = new Intent(LoginActivity.this, activityClass);
+
+                startActivity(intent);
+            }
+        });
+
+
+        logUserIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // call validation
+
+                // call login user
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/farmmanagerhelper/MainActivity.java
+++ app/src/main/java/com/example/farmmanagerhelper/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.farmmanagerhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
 
         // heres a comment
         // Repo tests
+
+        Button LoginActivity = findViewById((R.id.LoginActivity));
+
+        LoginActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Class activityClass = com.example.farmmanagerhelper.LoginActivity.class;
+                Intent intent = new Intent(MainActivity.this, com.example.farmmanagerhelper.LoginActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+//        // takes in a class passed to it and opens it
+//        private void openActivity(Class activityClass) {
+//            Intent intent = new Intent(this, activityClass);
+//            startActivity(intent);
+//        }
     }
 }
